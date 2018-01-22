d86c162b2b76ca2b3f82bea5557523a5b7a9cd5b
Pepito <benoit.messiaen@ensg.eu>
2018-01-22 16:10:58 +0100

    v3
    ordre boutons

--- php/main_public.php
+++ php/main_public.php
@@ -3,7 +3,7 @@
 <header>
   <section id="menu" draggable="true">
     <?php
-    $pages = array("accueil"=>"Accueil", "admissible"=>"Admissible", "bde"=>"Le BDE", "bds"=>"Le BDS", "actu"=>"Actualités", "ignare"=>"L'IGNare", "album"=>"Photos", "contact"=>"Contacts");
+    $pages = array("accueil"=>"Accueil", "admissibles"=>"Admissibles", "bde"=>"Le BDE", "bds"=>"Le BDS", "actu"=>"Actualités", "ignare"=>"L'IGNare", "album"=>"Photos", "contact"=>"Contacts");
     foreach ($pages as $nom => $page) {
       ?>
       <button class="bouton" target="_self" onmouseup="goToPHP('./php/<?php echo $nom; ?>.php')"><span><strong><?php echo $page; ?></strong></span></button>
