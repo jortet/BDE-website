05e8a494b110b4b5cf6813304fddcf22230796ab
Pepito <benoit.messiaen@ensg.eu>
2018-01-29 10:19:25 +0100

    contacts

---
+++ php/contact.php
@@ -0,0 +1,9 @@
+<?php if (!isset($_SESSION)) {session_start();} ?>
+<?php include("redirect.php"); ?>
+<p>Site de l'ENSG Géomatique: <a target="_blank" href="http://ensg.eu">ensg.eu</a></p>
+<p>Site de l'IGN: <a target="_blank" href="http://ign.fr">ign.fr</a></p>
+<p>Site de Vertigeo, junior-entreprise de l'école : <a target"_blank" href="http://vertigeo.ensg.eu">vertigeo.ensg.eu</a><p>
+<p>La plaquette du cycle ingénieur est disponible en téléchargement <a href="./docs/plaquette_cycle_ingenieur_2017.pdf" download = "plaquette_cycle_ingenieur_ENSG_2017">ici</a> (5Mo).</p>
+<p>La plaquette alpha 2017 de l'ENSG est disponible en téléchargement <a href = "./docs/plaquette_alpha_ensg_2017.pdf" download = "plaquette_alpha_ENSG_2017">ici</a> (25Mo).<br><br></p>
+
+<p>Si tu as des questions à propos de l'école, n'hésite pas à nous envoyer un mail à l'adresse bde@ensg.eu</p>
